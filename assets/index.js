a30c6a35d76a26977278f958435a560a64a88d17
AdhikariDarpan <124805232+AdhikariDarpan@users.noreply.github.com>
2025-03-16 19:35:12 +0545

    Update index.js
    background change

--- assets/index.js
+++ assets/index.js
@@ -175,20 +175,19 @@ function downloadHTML() {
   ${bodyContent}
   ${js ? `<script>\n${js}\n</script>` : ""}
   <script>
-   const colors = ["blue", "purple", "red", "green", "orange","grey","white","black"];
- let index = 0;
- function animateConsoleLog() {
-     console.clear();
-     console.log(
-         'https://darpanadhikari.com.np',
-         'font-size: 40px; color: #fff; text-shadow: 2px 2px 4px rgba(0,0,0,0.4); 
-         background: linear-gradient(to right, ${colors[index]}, ${colors[(index + 1) % colors.length]}); 
-         padding: 10px;'
-     );
-     index = (index + 1) % colors.length;
-     setTimeout(animateConsoleLog, 500);
- }
- animateConsoleLog();
+   const colors = ["blue", "purple", "red", "green", "orange", "grey", "white", "black"];
+   let index = 0;
+   function animateConsoleLog() {
+       console.clear();
+       console.log(
+           "%chttps://darpanadhikari.com.np",
+           "font-size: 40px; color: #fff; text-shadow: 2px 2px 4px rgba(0,0,0,0.4); " +
+           "background: " + colors[index] + "; padding: 10px;"
+       );
+       index = (index + 1) % colors.length;
+       setTimeout(animateConsoleLog, 500); // Change every 500ms
+   }
+   animateConsoleLog();
   </script>
 </body>
 </html>`;
